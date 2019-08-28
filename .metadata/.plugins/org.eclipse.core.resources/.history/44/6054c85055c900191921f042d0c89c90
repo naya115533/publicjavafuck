6fdbd67ced732b542c0885a6dd09f48cc4dbf41f
naya115533 <54258785+naya115533@users.noreply.github.com>
2019-08-28 14:41:10 +0900

    nayana

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/44/6054c85055c900191921f042d0c89c90
@@ -0,0 +1,31 @@
+
+public class hello {
+
+	public static void hap(int a,int b)
+	{
+		int s = a+b;
+		
+		System.out.println("ÇÕ="+s);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5, b=7;
+		hap(a,b); cha(a,b);
+		World.gop(a,b);
+	}
+	public static void cha(int a,int b)
+	{
+		int c = a-b;
+		
+		System.out.println("Â÷"+c);
+	}
+}
+class World
+{
+	public static void gop(int a,int b)
+	{
+		int g = a*b;
+		
+		System.out.println("°ö="+g);
+	}
+}
--- naya/src/hello.java
+++ naya/src/hello.java
@@ -9,15 +9,34 @@ public class hello {
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a=5, b=7;
+		int a=5, b=7, c=9;
 		hap(a,b); cha(a,b);
 		World.gop(a,b);
+		System.out.println("Че="+sum(a,b,c));
+		System.out.println("Че="+sum1(a,b,c));
+		System.out.println("ЦђБе="+avg(a,b,c));
+		System.out.println("ЦђБе="+World.avg(a,b,c));
 	}
 	public static void cha(int a,int b)
 	{
 		int c = a-b;
 		
 		System.out.println("Тї"+c);
+	}
+
+	public static int sum(int a,int b,int c)
+	{
+		int sum = a+b+c;
+		
+		return sum;
+	}
+	public static int sum1(int a,int b,int c)
+	{
+		return a+b+c;
+	}
+	public static float avg(int a,int b,int c)
+	{
+		return (a+b+c)/3;
 	}
 }
 class World
@@ -28,4 +47,11 @@ class World
 		
 		System.out.println("Аі="+g);
 	}
+	public static float avg(int a,int b,int c)
+	{
+		float avg = (a+b+c)/3;
+		
+		return avg;
+	}
 }
+	
